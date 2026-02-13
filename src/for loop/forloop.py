4e73e41168aeb1bb8795b9db268ccc9d3c619b48
imsuryya <suryar12130@gmail.com>
2026-02-13 13:09:20 +0530

    commit

--- src/for loop/forloop.py
+++ src/for loop/forloop.py
@@ -4,7 +4,22 @@
 #     print(i)
 
 # Basic for loop Using String
-fruits = ["apple", "banana", "mango"]
+# fruits = ["apple", "banana", "mango"]
 
-for fruit in fruits:
-    print(fruit)
+# for fruit in fruits:
+#     print(fruit)
+
+n = int(input("Enter a number: "))
+
+for num in range(2, n + 1):
+    is_prime = True
+
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(num, "is a Prime Number")
+    else:
+        print(num, "is Not a Prime Number")
--- src/for loop/whileloop.py
+++ src/for loop/whileloop.py
@@ -1,7 +1,27 @@
 
 # Basic while loop using integer
-i = 1
+# i = 1
 
-while i <= 5:
-    print(i)
-    i += 1
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+n = int(input("Enter a number: "))
+
+num = 2
+while num <= n:
+    i = 2
+    is_prime = True
+
+    while i <= int(num ** 0.5):
+        if num % i == 0:
+            is_prime = False
+            break
+        i += 1
+
+    if is_prime:
+        print(num, "is a Prime Number")
+    else:
+        print(num, "is Not a Prime Number")
+
+    num += 1
